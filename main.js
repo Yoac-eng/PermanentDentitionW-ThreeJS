befab3d68fde22887ebc8b3a18b8a5e1965d8458
Cayo Cutrim <cayocutrim1991@gmail.com>
2023-08-28 17:16:06 -0300

    fixing method call

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
-const renderer = new THREE.WebGL1Renderer({ antialias: true });
+// Setting up the renderer
+const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.outputColorSpace = THREE.SRGBColorSpace;
 
 renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setClearColor(0xf3f);
+renderer.setClearColor(0x000000);
 renderer.setPixelRatio(window.devicePixelRatio);
 
 document.body.appendChild(renderer.domElement);
@@ -19,27 +20,17 @@ camera.position.set(4, 5, 11);
 camera.lookAt(0, 0, 0);
 
 //Adding some geometry to make sure the scene is working properly
-// const groundGeometry = new THREE.PlaneGeometry(20, 20, 32, 32);
-// groundGeometry.rotateX(-Math.PI / 2);
-// const groundMaterial = new THREE.MeshStandardMaterial({
-//   color: 0x555555,
-//   side: THREE.DoubleSide
-// });
-// const groundMesh = new THREE.Mesh(groundGeometry, groundMaterial);
-// scene.add(groundMesh);
+const groundGeometry = new THREE.PlaneGeometry(20, 20, 32, 32);
+groundGeometry.rotateX(-Math.PI / 2);
+const groundMaterial = new THREE.MeshStandardMaterial({
+  color: 0x555555,
+  side: THREE.DoubleSide
+});
+const groundMesh = new THREE.Mesh(groundGeometry, groundMaterial);
+scene.add(groundMesh);
 
-const spotLight = new THREE.SpotLight( 0xffffff );
-spotLight.position.set( 100, 1000, 100 );
-
-spotLight.castShadow = true;
-
-spotLight.shadow.mapSize.width = 1024;
-spotLight.shadow.mapSize.height = 1024;
-
-spotLight.shadow.camera.near = 500;
-spotLight.shadow.camera.far = 4000;
-spotLight.shadow.camera.fov = 30;
-
+const spotLight = new THREE.SpotLight( 0xffffff, 3, 100, 0.2, 0.5);
+spotLight.position.set( 0, 25, 0);
 scene.add(spotLight);
 
 const loader = new GLTFLoader();
